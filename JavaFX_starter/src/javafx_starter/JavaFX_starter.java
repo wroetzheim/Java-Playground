5098d0052ff6178ff8c7c6eb268e71f81fe112ca
tjrotz <tjrotz@gmail.com>
2015-08-26 19:00:33 -0700

    Messing around with shapes

--- JavaFX_starter/src/javafx_starter/JavaFX_starter.java
+++ JavaFX_starter/src/javafx_starter/JavaFX_starter.java
@@ -5,14 +5,17 @@
  */
 package javafx_starter;
 
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.shape.*;
+
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 import javafx.scene.text.*;
 
 
@@ -22,12 +25,12 @@ public class JavaFX_starter extends Application {
     public void start(Stage primaryStage) {
         Text reply = new Text();
         reply.setText("Peanuts!");
+        
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600);
+        
         Button a_button = new Button();
         a_button.setText("Favorite Nut?");
-        
-        Pane left;
-        left = new Pane();
-
         a_button.setOnAction(new EventHandler<ActionEvent>() {
             
             int x = 10;
@@ -43,10 +46,31 @@ public class JavaFX_starter extends Application {
             }
         });
 
-        left.getChildren().add(a_button);
-        left.getChildren().add(reply);
+        Group circles = new Group();
+        
+        for (int i = 0; i < 30; i++) {
+            Circle circle = new Circle();
+            circle.setCenterX(10.0);
+            circle.setCenterY(10.0);
+            circle.setRadius(5.0);
+            circles.getChildren().add(circle);
+        }
+        
+        Ellipse circle = EllipseBuilder.create()
+             .centerX(500)
+             .centerY(500)
+             .radiusX(30)
+             .radiusY(30)
+             .strokeWidth(3)
+             .stroke(Color.YELLOW)
+             .fill(Color.TEAL)
+             .build();
+        
+        root.getChildren().add(a_button);
+        root.getChildren().add(reply);
+        root.getChildren().add(circle);
         a_button.relocate(70,70);
-        Scene scene = new Scene(left, 500, 450);
+        
         
         primaryStage.setTitle("A peanut's favorite nut");
         primaryStage.setScene(scene);
